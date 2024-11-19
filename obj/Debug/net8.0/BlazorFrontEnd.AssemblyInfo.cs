516cdfc27b4e4521995c9de944146e41196d6325
Jose <josepalacio329201@correo.itm.edu.co>
2024-11-18 19:41:28 -0500

    Actualización del Nav e implementación de ValidacionAcceso a tabla Usuarios.

--- obj/Debug/net8.0/BlazorFrontEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorFrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorFrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9027f041aadbad61bc6e82f97a675f2a4fc5e3d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ec8ec44d54812479eb3a9c1847433b8e609606")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorFrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorFrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
