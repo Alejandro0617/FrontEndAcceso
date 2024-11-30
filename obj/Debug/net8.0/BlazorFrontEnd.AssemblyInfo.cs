22d55c234b51e4850468b81940f19c30280686f9
Alejandro0617 <162916224+Alejandro0617@users.noreply.github.com>
2024-11-29 23:22:43 -0500

    Rama modificador1 (#2)
    * Apartado visual para tabla intermedia 'docente_departamento' en la tabla principal 'docente'
    
    * Misma correción del apartado visual en tablas principales e intermedias.
    
    * Actualización del Nav e implementación de ValidacionAcceso a tabla Usuarios.
    
    ---------
    
    Co-authored-by: Jose <josepalacio329201@correo.itm.edu.co>

--- obj/Debug/net8.0/BlazorFrontEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorFrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorFrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b09b7dcef84a9972e581d22c73dca2d1889b69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdf34178c1f086912f6b2ef1200e86eb35b0d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorFrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorFrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
